ef7097a1de0c155945c7b2e326af651cbb11c8fc
Anton Cherepov <chekitdnb@gmail.com>
2021-08-17 00:17:40 +0200

    Added MaterialUI and Loft Taxi Theme

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// Theme
+import { theme } from "loft-taxi-mui-theme";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
