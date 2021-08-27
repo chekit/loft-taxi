cb75b0fe8fee00ead484fb7cbe55caa519cd5575
Anton Cherepov <chekitdnb@gmail.com>
2021-08-27 15:20:49 +0200

    Fixed tests

--- src/pages/Profile/Profile.spec.jsx
+++ src/pages/Profile/Profile.spec.jsx
@@ -3,9 +3,10 @@ import { Profile } from './Profile';
 import { render, screen } from '@testing-library/react';
 
 describe('Profile Page', () => {
+    it.todo('Add Full test coverage');
     it(`renders`, () => {
         render(<Profile />);
-        const content = screen.getByText(/Profile Page/i);
+        const content = screen.getByText(/Профиль/i);
         expect(content).toBeInTheDocument();
     });
 });
