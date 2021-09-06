60598c006edc2ef5ac6cb5802e7a73c50a42d15e
Anton Cherepov <chekitdnb@gmail.com>
2021-09-06 12:29:55 +0200

    Added data to auth service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,4 +1,31 @@
 export class AuthService {
-    login() {}
-    logout() {}
+    baseURL = `https://loft-taxi.glitch.me/`;
+
+    async register(data) {
+        try {
+            const res = await fetch(`${this.baseURL}/register`, {
+                method: 'POST',
+                body: JSON.stringify(data)
+            });
+
+            return await res.json();
+        } catch (e) {
+
+        }
+    }
+
+    async login(data) {
+        try {
+            const res = await fetch(`${this.baseURL}/auth`, {
+                method: 'POST',
+                body: JSON.stringify(data)
+            });
+
+            return await res.json();
+        } catch (e) {
+
+        }
+    }
+
+    logout() { }
 }
