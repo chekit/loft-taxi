fa0c32329dc2b125168f6810c1d83dc7a54281d9
Anton Cherepov <chekitdnb@gmail.com>
2021-09-11 20:59:01 +0200

    Fix

--- src/components/Forms/RegistrationForm/RegistrationForm.jsx
+++ src/components/Forms/RegistrationForm/RegistrationForm.jsx
@@ -48,7 +48,7 @@ class RegistrationForm extends PureComponent {
             <Form title="Регистрация" submitHandler={this.submitHandler} testId={REGISTRATION_FORM_TEST_ID}>
                 <fieldset className="form__fieldset">
                     <FormInput label="Email" type="email" name="email" placeholder="mail@mail.ru" value={email} onChangeHandler={this.handleInputChange} isRequired={true} />
-                    <FormInput label="Как вас зовут?" type="text" name="name" placeholder="Петр Александрович" value={name} onChangeHandler={this.handleInputChange} isRequired={true} />
+                    <FormInput label="Как вас зовут?" name="name" placeholder="Петр Александрович" value={name} onChangeHandler={this.handleInputChange} isRequired={true} />
                     <FormInput label="Придумайте пароль" type="password" name="password" placeholder="********" value={password} onChangeHandler={this.handleInputChange} isRequired={true} />
                 </fieldset>
                 <SubmitButton title="Зарегистрироваться" isDisabled={!email || !password || !name}></SubmitButton>
