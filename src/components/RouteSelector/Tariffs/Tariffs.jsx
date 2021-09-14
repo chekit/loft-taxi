ab78610a1645eaed4fb90c6fcd32be1e73d2f40a
Anton Cherepov <chekitdnb@gmail.com>
2021-09-15 00:21:30 +0200

    Added tariff selection

--- src/components/RouteSelector/Tariffs/Tariffs.jsx
+++ src/components/RouteSelector/Tariffs/Tariffs.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tariff from './Tariff';
 
 import './Tariffs.scss';
@@ -7,17 +7,25 @@ import standart from './../../../assets/cars/standart.png';
 import premium from './../../../assets/cars/premium.png';
 import business from './../../../assets/cars/business.png';
 
+export const TariffsTypes = {
+    STANDART: 'standart',
+    PREMIUM: 'premium',
+    BUSINESS: 'business',
+}
+
 export const Tariffs = () => {
+    const [selectedTariff, setTariff] = useState(TariffsTypes.STANDART);
+
     return (
         <div className="tariffs">
-            <div className="tariffs__item is-active">
-                <Tariff title="Стандарт" price="150" visual={standart} />
+            <div className={`tariffs__item ${ selectedTariff === TariffsTypes.STANDART ? 'is-active': '' }`}>
+                <Tariff title="Стандарт" price="150" handleSelect={() => setTariff(TariffsTypes.STANDART)} visual={standart} />
             </div>
-            <div className="tariffs__item">
-                <Tariff title="Премиум" price="250" visual={premium} />
+            <div className={`tariffs__item ${selectedTariff === TariffsTypes.PREMIUM ? 'is-active': '' }`}>
+                <Tariff title="Премиум" price="250" handleSelect={() => setTariff(TariffsTypes.PREMIUM)} visual={premium} />
             </div>
-            <div className="tariffs__item">
-                <Tariff title="Бизнес" price="300" visual={business} />
+            <div className={`tariffs__item ${selectedTariff === TariffsTypes.BUSINESS ? 'is-active': '' }`}>
+                <Tariff title="Бизнес" price="300" handleSelect={() => setTariff(TariffsTypes.BUSINESS)} visual={business} />
             </div>
         </div>
     );
