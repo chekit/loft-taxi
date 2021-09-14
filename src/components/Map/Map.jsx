7b100156ae4a5a2082e86a19b954e039fc07b4a1
Anton Cherepov <chekitdnb@gmail.com>
2021-09-14 23:53:08 +0200

    Added drawing way logic

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,9 @@
-import React, { createRef, useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import './Map.scss';
+import { useStore } from 'react-redux';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOXGL_ACCESS_TOKEN;
 
@@ -12,21 +13,72 @@ const DEFAULTS = {
     zoom: 13.3
 };
 
+const ROUTE_LAYER_ID = 'route';
+
+const getSourceData = coordinates => ({
+    type: "Feature",
+    properties: {},
+    geometry: {
+        type: "LineString",
+        coordinates
+    }
+});
+
+const drawRoute = (map, coordinates) => {
+    map.flyTo({
+        center: coordinates[0],
+        zoom: 13.3
+    });
+
+    if (map.isStyleLoaded()) {
+        const data = getSourceData(coordinates);
+
+        if (map.getLayer(ROUTE_LAYER_ID)) {
+            map.getSource(ROUTE_LAYER_ID).setData(data)
+        } else {
+            map.addLayer({
+                id: ROUTE_LAYER_ID,
+                type: "line",
+                source: {
+                    type: "geojson",
+                    data
+                },
+                layout: {
+                    "visibility": "visible",
+                    "line-join": "round",
+                    "line-cap": "round"
+                },
+                paint: {
+                    "line-color": "#ffc617",
+                    "line-width": 8
+                }
+            });
+        }
+    }
+};
+
 export const Map = () => {
-    const mapContainerRef = createRef(null);
-    const { lng, lat, zoom } = DEFAULTS;
+    const mapContainerRef = useRef(null);
+    const map = useRef(null);
+
+    const store = useStore();
+    const { currentRoute } = store.getState();
 
     useEffect(() => {
-        const map = new mapboxgl.Map({
-            container: mapContainerRef.current,
-            style: 'mapbox://styles/chekit/cksksm53n20af17pjblj9oo1a',
-            center: [lng, lat],
-            zoom,
-        });
+        if (!map.current) {
+            const { lng, lat, zoom } = DEFAULTS;
 
-        // Unmount
-        return () => map.remove();
-    });
+            map.current = new mapboxgl.Map({
+                container: mapContainerRef.current,
+                style: 'mapbox://styles/chekit/cksksm53n20af17pjblj9oo1a',
+                center: [lng, lat],
+                zoom,
+            });
+        }
+
+        drawRoute(map.current, currentRoute);
+    }, [currentRoute, mapContainerRef]);
+
 
     return (
         <div className="map-container" ref={mapContainerRef}></div>
